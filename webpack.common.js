dc828b2480307b8f22b25da729dc1364b54307f1
kuncoroadjie <kuncoroadjie17@gmail.com>
2023-01-30 20:18:06 +0700

    adding service worker

--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,7 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
+    sw: path.resolve(__dirname, 'src/scripts/sw.js'),
   },
   output: {
     filename: '[name].bundle.js',
